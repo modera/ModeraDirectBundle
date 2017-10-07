175823ee772f7f3af37c3cc159e5553b55a8e4ed
Sergei Lissovski <sergei.lissovski@gmail.com>
2017-10-07 14:22:11 +0300

    referer validation for preventing CSRF

--- Controller/DirectController.php
+++ Controller/DirectController.php
@@ -3,6 +3,7 @@
 namespace Modera\DirectBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Modera\DirectBundle\Api\Api;
 use Modera\DirectBundle\Router\Router;
@@ -14,22 +15,30 @@ class DirectController extends Controller
      *
      * @return Response
      */
-    public function getApiAction()
+    public function getApiAction(Request $request)
     {
+        $this->validateRequest($request);
+
         // instantiate the api object
         $api = new Api($this->container);
 
         $debug = $this->container->get('kernel')->isDebug();
 
         if ($debug) {
-            $exceptionLogStr =
+            $exceptionLogStmt =
                 'Ext.direct.Manager.on("exception", function(error){console.error(Ext.util.Format.format("Remote Call: {0}.{1}\n{2}", error.action, error.method, error.message, error.where)); return false;});';
         } else {
-            $exceptionLogStr =
+            $exceptionLogStmt =
                 sprintf('Ext.direct.Manager.on("exception", function(error){console.error("%s");});', $this->container->getParameter('direct.exception.message'));
         }
+
+        $js = $this->renderView('@ModeraDirect/Direct/api.js.twig', array(
+            'api' => $api,
+            'exceptionLogStmt' => $exceptionLogStmt,
+        ));
+
         // create the response
-        $response = new Response(sprintf('Ext.Direct.addProvider(%s);%s', $api, $exceptionLogStr));
+        $response = new Response($js);
         $response->headers->set('Content-Type', 'text/javascript');
 
         return $response;
@@ -59,8 +68,10 @@ class DirectController extends Controller
      *
      * @return Response
      */
-    public function routeAction()
+    public function routeAction(Request $request)
     {
+        $this->validateRequest($request);
+
         // instantiate the router object
         $router = new Router($this->container);
 
@@ -69,5 +80,36 @@ class DirectController extends Controller
         $response->headers->set('Content-Type', 'application/json');
 
         return $response;
+    }
+
+    private function validateRequest(Request $request)
+    {
+        if ($request->headers->has('referer')) {
+            $allowedReferers = $this->getAllowedReferers();
+            if (count($allowedReferers) == 0) {
+                return;
+            }
+
+            $referer = $request->headers->get('referer');
+
+            $isAllowed = false;
+            foreach ($allowedReferers as $allowedReferer) {
+                if (preg_match($allowedReferer, $referer)) {
+                    $isAllowed = true;
+
+                    break;
+                }
+            }
+
+            if (!$isAllowed) {
+                throw new \RuntimeException('Given referer is not in whitelist.');
+            }
+        }
+    }
+
+    private function getAllowedReferers()
+    {
+        return [
+        ];
     }
 }
